b68c889a227dcceb8e0c405de7d30d2b93138c5e
Brandon Quach <brandonquach@Michaels-MBP-2.gateway.ht.net>
2020-06-04 00:50:03 -1000

    Added some comments

--- eval_performance.py
+++ eval_performance.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import sys
 from contextlib import contextmanager
 
-
+# Context manager for surpressing output of the tests into a log file
 @contextmanager
 def custom_redirection(fileobj):
     old = sys.stdout
@@ -16,9 +16,13 @@ def custom_redirection(fileobj):
         sys.stdout = old
 
 
+# Number of trials to run the qdrift algorithm
 N = 100
+
+# Stores the resulting fidelities with ground truth 
 fidelity_qdrift = []
 fidelity_suzuki = []
+
 pbar = tqdm(range(N))
 for _ in pbar:
     with open('log.txt', 'w') as out:
@@ -29,10 +33,13 @@ for _ in pbar:
     pbar.set_description("Average qdrift fidelity: {} Average suzuki fidelity: {}"\
         .format(np.mean(fidelity_qdrift), np.mean(fidelity_suzuki)))
 
+# Print resulting average fidelity
 print("Average qdrift fidelity:", np.mean(fidelity_qdrift))
 print("Average suzuki fidelity:", np.mean(fidelity_suzuki))
 
 print("Saving Results!")
+
+# Save results
 np.save("qdrift_fidelities.npy", fidelity_qdrift)
 np.save("suzuki_fidelities.npy", fidelity_suzuki)
 
