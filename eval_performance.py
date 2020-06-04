a36de82ae8e1f2be58782d0ec7ead00524598eef
Brandon Quach <brandonquach@Michaels-MBP-2.gateway.ht.net>
2020-06-03 19:05:06 -1000

    Made output of evaluation code nicer

--- eval_performance.py
+++ eval_performance.py
@@ -1,13 +1,30 @@
 from test import run_exp 
 import numpy as np
+from tqdm import tqdm 
+
+import sys
+from contextlib import contextmanager
+
+
+@contextmanager
+def custom_redirection(fileobj):
+    old = sys.stdout
+    sys.stdout = fileobj
+    try:
+        yield fileobj
+    finally:
+        sys.stdout = old
+
 
 N = 100
 fidelity_qdrift = []
 fidelity_suzuki = []
-for _ in range(N):
-    res_qdrift, res_suzuki = run_exp()
-    fidelity_qdrift.append(res_qdrift)
-    fidelity_suzuki.append(res_suzuki)
+for _ in tqdm(range(N)):
+    with open('log.txt', 'w') as out:
+        with custom_redirection(out):
+            res_qdrift, res_suzuki = run_exp()
+            fidelity_qdrift.append(res_qdrift)
+            fidelity_suzuki.append(res_suzuki)
 
 print("Average qdrift fidelity:", np.mean(fidelity_qdrift))
 print("Average suzuki fidelity:", np.mean(fidelity_suzuki))
